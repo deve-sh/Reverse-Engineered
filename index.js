1583036c162f0d1a2e4b7caf5d38923b8d16d3b5
Devesh Kumar <devesh2027@gmail.com>
2021-08-15 00:23:18 +0530

    Made all necessary variables and methods private.
    Also added a createStore function to create a singleton instance of the store.

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 class Redux {
-    #state = {};
+	#state = {};
+	#reducer = () => null;
+	#subscribers = [];
+	#middlewares = [];
 
 	constructor(reducer, initialState) {
 		if (
@@ -15,19 +18,19 @@ class Redux {
 			throw new Error("Reducer and initialState are required.");
 
 		this.#state = initialState || {};
-		this.reducer = reducer;
-		this.subscribers = [];
-		this.middlewares = [];
+		this.#reducer = reducer;
+		this.#subscribers = [];
+		this.#middlewares = [];
 	}
 
 	getState() {
 		return this.#state;
 	}
 
-	notifySubscribers() {
-		if (this.subscribers.length) {
+	#notifySubscribers() {
+		if (this.#subscribers.length) {
 			// Notify subscribers of change to state.
-			for (let func of this.subscribers) func(this.getState());
+			for (let func of this.#subscribers) func(this.getState());
 		}
 	}
 
@@ -35,21 +38,29 @@ class Redux {
 	#setState(newState) {
 		if (!newState || !newState instanceof Object) return;
 		this.#state = newState;
-		this.notifySubscribers();
+		this.#notifySubscribers();
 	}
 
 	dispatch(action) {
-		if (this.reducer instanceof Function) {
-			this.#setState(this.reducer(this.#state, action));
+		if (this.#reducer instanceof Function) {
+			this.#setState(this.#reducer(this.#state, action));
 		}
 	}
 
 	subscribe(callbackFunction) {
 		// Check if functiion has already been added to list of subscribers.
-		for (let i = 0; i < this.subscribers.length; i++)
-			if (this.subscribers[i] === callbackFunction) return;
-		this.subscribers.push(callbackFunction);
+		for (let i = 0; i < this.#subscribers.length; i++)
+			if (this.#subscribers[i] === callbackFunction) return;
+		this.#subscribers.push(callbackFunction);
 	}
 }
 
 // Export a singleton instance of the above class. So each part of the app has access to only one instance.
+let instance = null;
+function createStore(reducer, initialState) {
+	if (instance) return instance;
+	else {
+		instance = new Redux(reducer, initialState);
+		return instance;
+	}
+}
