dc64de02ab340a4577ea2310aa4107f63902309e
Devesh Kumar <devesh2027@gmail.com>
2021-08-15 12:04:49 +0530

    Redux: Added support for key-based multiple reducers.

--- Redux/index.js
+++ Redux/index.js
@@ -4,21 +4,24 @@
 
 class Redux {
 	#state = {};
-	#reducer = () => null;
+	#reducers = {};
 	#subscribers = [];
 
-	constructor(reducer, initialState) {
-		if (
-			!reducer ||
-			!initialState ||
-			!reducer instanceof Function ||
-			!initialState instanceof Object
-		)
-			throw new Error("Reducer and initialState are required.");
+	constructor(reducers, initialState) {
+		if (!reducers || !initialState || !initialState instanceof Object)
+			throw new Error("Reducers and initialState are required.");
 
 		this.#state = initialState || {};
-		this.#reducer = reducer;
+		this.#reducers = reducers || (() => null);
 		this.#subscribers = [];
+	}
+
+	#isSingleReducer() {
+		return (
+			this.#reducers instanceof Function ||
+			!this.#reducers instanceof Object ||
+			Object.keys(this.#reducers).length === 0
+		);
 	}
 
 	getState() {
@@ -40,9 +43,20 @@ class Redux {
 	}
 
 	dispatch(action) {
-		if (this.#reducer instanceof Function) {
-			this.#setState(this.#reducer(this.#state, action));
+		let newState = this.getState();
+
+		if (!this.#isSingleReducer()) {
+			for (let reducer in this.#reducers) // Applying action based on each reducer.
+				if (this.#reducers[reducer] instanceof Function)
+					newState[reducer] = this.#reducers[reducer](this.getState(), action);
+		} else {
+			newState =
+				this.#reducers instanceof Function
+					? this.#reducers(this.#state, action)
+					: newState;
 		}
+
+		this.#setState(newState);
 	}
 
 	subscribe(callbackFunction) {
