54c96c989fc47236b3d618b182bb578c792bb3cc
Devesh Kumar <devesh2027@gmail.com>
2021-08-15 12:46:33 +0530

    Redux: Updated test conditions for single and multiple reducers.

--- Redux/tests.js
+++ Redux/tests.js
@@ -2,11 +2,13 @@
  * Tests for Redux Clone
  */
 
-console.group();
 const INCREMENT_COUNT = "INCREMENT_COUNT";
 const DECREMENT_COUNT = "DECREMENT_COUNT";
 
-let reducer = (state = { count: 0 }, action = { type: INCREMENT_COUNT }) => {
+let firstReducer = (
+	state = { count: 0 },
+	action = { type: INCREMENT_COUNT }
+) => {
 	switch (action.type) {
 		case INCREMENT_COUNT:
 			return { count: (state.count || 0) + 1 };
@@ -17,19 +19,69 @@ let reducer = (state = { count: 0 }, action = { type: INCREMENT_COUNT }) => {
 	}
 };
 
-let store = createStore(reducer);
+/**
+ * Multiple Reducer tests
+ */
+const SET_TEXT = "SET_TEXT";
+let otherReducer = (
+	state = { text: "" },
+	action = { type: SET_TEXT, text: "" }
+) => {
+	switch (action.type) {
+		case SET_TEXT:
+			return { text: action.text || "" };
+		default:
+			return state;
+	}
+};
 
-console.log("Initial State: ", store.getState());
-console.log(
-	"Dispatching: ",
-	INCREMENT_COUNT,
-	store.dispatch({ type: INCREMENT_COUNT })
-);
-console.log("Updated State: ", store.getState());
-console.log(
-	"Dispatching: ",
-	DECREMENT_COUNT,
-	store.dispatch({ type: DECREMENT_COUNT })
-);
-console.log("Updated State: ", store.getState());
-console.groupEnd();
+const singleReducerTests = () => {
+	let store = createStore(firstReducer);
+
+	console.group();
+	console.log("Initial State: ", store.getState());
+	console.log(
+		"Dispatching: ",
+		INCREMENT_COUNT,
+		store.dispatch({ type: INCREMENT_COUNT })
+	);
+	console.log("Updated State: ", store.getState());
+	console.log(
+		"Dispatching: ",
+		DECREMENT_COUNT,
+		store.dispatch({ type: DECREMENT_COUNT })
+	);
+	console.log("Updated State: ", store.getState());
+	console.groupEnd();
+};
+
+const multipleReducerTests = () => {
+	let store = createStore(
+		combineReducers({
+			countState: firstReducer,
+			textState: otherReducer,
+		})
+	);
+
+	console.log("Initial State: ", store.getState());
+	console.log(
+		"Dispatching: ",
+		INCREMENT_COUNT,
+		store.dispatch({ type: INCREMENT_COUNT })
+	);
+	console.log("Updated State: ", store.getState());
+	console.log(
+		"Dispatching: ",
+		DECREMENT_COUNT,
+		store.dispatch({ type: DECREMENT_COUNT })
+	);
+	console.log("Updated State: ", store.getState());
+
+	console.log(
+		"Dispatching: ",
+		SET_TEXT,
+		store.dispatch({ type: SET_TEXT, text: "Updated Text" })
+	);
+	console.log("Updated State: ", store.getState());
+	console.groupEnd();
+};
